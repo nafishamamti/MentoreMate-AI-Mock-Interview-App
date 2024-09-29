2d5d8fbe94b17402607ac1932a95bc94ddabda92
Nafisha Mamti <nafisha.111@gmail.com>
2024-09-28 20:08:13 -0400

    Initial Commit

---
+++ MentorMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MentorMate
+//
+//  Created by Radhika on 9/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MentorMateTests/MentorMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  MentorMateTests.swift
+//  MentorMateTests
+//
+//  Created by Radhika on 9/28/24.
+//
+
+import Testing
+@testable import MentorMate
+
+struct MentorMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
